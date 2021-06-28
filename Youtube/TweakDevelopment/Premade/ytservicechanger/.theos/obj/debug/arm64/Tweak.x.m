a9dc98f002e5485c3dcb27ae63e855e7c790f4f0
Sam Laister <samlaister@Sams-MacBook-Air.local>
2021-06-28 13:58:32 +0100

    Added tweak development

---
+++ Youtube/TweakDevelopment/Premade/ytservicechanger/.theos/obj/debug/arm64/Tweak.x.m
@@ -0,0 +1,43 @@
+#line 1 "Tweak.x"
+#import "Tweak.h"
+
+
+#include <substrate.h>
+#if defined(__clang__)
+#if __has_feature(objc_arc)
+#define _LOGOS_SELF_TYPE_NORMAL __unsafe_unretained
+#define _LOGOS_SELF_TYPE_INIT __attribute__((ns_consumed))
+#define _LOGOS_SELF_CONST const
+#define _LOGOS_RETURN_RETAINED __attribute__((ns_returns_retained))
+#else
+#define _LOGOS_SELF_TYPE_NORMAL
+#define _LOGOS_SELF_TYPE_INIT
+#define _LOGOS_SELF_CONST
+#define _LOGOS_RETURN_RETAINED
+#endif
+#else
+#define _LOGOS_SELF_TYPE_NORMAL
+#define _LOGOS_SELF_TYPE_INIT
+#define _LOGOS_SELF_CONST
+#define _LOGOS_RETURN_RETAINED
+#endif
+
+@class _UIStatusBarStringView; 
+static void (*_logos_orig$_ungrouped$_UIStatusBarStringView$setText$)(_LOGOS_SELF_TYPE_NORMAL _UIStatusBarStringView* _LOGOS_SELF_CONST, SEL, id); static void _logos_method$_ungrouped$_UIStatusBarStringView$setText$(_LOGOS_SELF_TYPE_NORMAL _UIStatusBarStringView* _LOGOS_SELF_CONST, SEL, id); 
+
+#line 3 "Tweak.x"
+
+
+static void _logos_method$_ungrouped$_UIStatusBarStringView$setText$(_LOGOS_SELF_TYPE_NORMAL _UIStatusBarStringView* _LOGOS_SELF_CONST __unused self, SEL __unused _cmd, id arg1) {
+	_logos_orig$_ungrouped$_UIStatusBarStringView$setText$(self, _cmd, arg1);
+	if ([arg1 isKindOfClass:[NSString class]] && ![arg1 containsString:@"%"] && ![arg1 containsString:@":"]) {
+		NSLog(@"Found: %@", arg1);
+
+		return _logos_orig$_ungrouped$_UIStatusBarStringView$setText$(self, _cmd, StatusLabel);
+	}
+}
+
+ 
+static __attribute__((constructor)) void _logosLocalInit() {
+{Class _logos_class$_ungrouped$_UIStatusBarStringView = objc_getClass("_UIStatusBarStringView"); { MSHookMessageEx(_logos_class$_ungrouped$_UIStatusBarStringView, @selector(setText:), (IMP)&_logos_method$_ungrouped$_UIStatusBarStringView$setText$, (IMP*)&_logos_orig$_ungrouped$_UIStatusBarStringView$setText$);}} }
+#line 15 "Tweak.x"
---
+++ Youtube/TweakDevelopment/Premade/ytservicechanger/Tweak.h
@@ -0,0 +1,1 @@
+NSString* StatusLabel = @"Hello YT!";
